7c60c526c4430596f6e6933def04cf16000c94f3
clausmie <claus.miesner@coremedia.com>
2020-10-09 18:13:45 +0200

    Add BubbleSort

---
+++ src/main/java/algorithms/BubbleSort.java
@@ -0,0 +1,46 @@
+package algorithms;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class BubbleSort {
+    public int[] bubbleSort(int[] numbers){
+        boolean changedSomething = false;
+        for (int i = 0; i < numbers.length; i++){
+            for (int j = i; j < numbers.length-i-1; j++) {
+                if(numbers[j] > numbers[j+1]){
+                    int tempInt = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = tempInt;
+                    changedSomething = true;
+                  //  System.out.println(arrayToString(numbers));
+                }
+            }
+
+            if (changedSomething){
+                changedSomething = false;
+            } else {
+                return numbers;
+            }
+        }
+        return numbers;
+    }
+    public String arrayToString(int[] numbers){
+       // String result = "[";
+        String numbersString = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
+        return "[" + numbersString + "]";
+
+
+
+       /* for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i] + ", ;
+        }
+        result.substring(0,result.length()-1);
+        result += ']';
+
+
+        return result; */
+    }
+}
---
+++ src/test/java/algorithms/BubbleSortTest.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BubbleSortTest {
+    @Test
+    public void testBubbleSort(){
+        BubbleSort sorter = new BubbleSort();
+        int[] valuesToSort = {10,5,100,99,97,130};
+        int[] sortedValues = {5,10,97,99,100,130};
+
+        assertArrayEquals(sortedValues,sorter.bubbleSort(valuesToSort));
+    }
+}
